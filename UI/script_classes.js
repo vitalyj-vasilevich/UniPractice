a869179d282222d4360f66c7dc78bee8953664bf
vitalyj-vasilevich <vitalyj-vasilevich@gmail.com>
2019-03-19 11:19:18 +0300

    edited classes, todo: fix validatePost function

--- UI/script_classes.js
+++ UI/script_classes.js
@@ -1,25 +1,235 @@
-class photoPostsCollection{
+let photoPosts = [
+    {
+        id : '1',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non so',
+        createdAt : new Date('2018-02-23T23:00:00'),
+        author : 'Vitaly Vasilevich',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '2',
+        description : 'vallis vel massa id, porttitor eleifend magna. Aliquam erat volutpat. Aliquam tempu',
+        createdAt : new Date('2019-02-23T23:00:00'),
+        author : 'Vitaly Vasilevich',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '3',
+        description : 'Aenean consectetur, arcu eu malesuada mollis, enim tortor consequat quam, vel feugiat nunc metus a velit. Vivamus viverra massa eu aliquet ultrices.',
+        createdAt : new Date('2020-02-23T23:00:00'),
+        author : 'Vitaly Vasilevich',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['flike4like', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '4',
+        description : 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+        createdAt : new Date('2019-02-23T13:09:00'),
+        author : 'Vitaly Vasilevich',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '5',
+        description : 'Donec vel ante placerat, sagittis dolor at, posuere sapien.',
+        createdAt : new Date('2019-12-23T23:00:00'),
+        author : 'Vitaly Vasilevich',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['gay', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '6',
+        description : 'Quisque velit nisl, accumsan at euismod sit amet, gravida sit amet massa. ',
+        createdAt : new Date('2019-02-25T20:15:00'),
+        author : 'Ivanov Ivan',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'university', 'anime', 'girls'],
+    },
+    {
+        id : '7',
+        description : 'Ut at eros vitae ipsum congue pharetra sit amet sit amet metus.',
+        createdAt : new Date('2018-05-23T23:00:00'),
+        author : 'Ivanov Ivan',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '8',
+        description : 'Nullam eget sapien justo. Aliquam auctor at eros vel varius. Curabitur pellentesque mi ipsum,',
+        createdAt : new Date('2019-02-01T23:00:00'),
+        author : 'Ivanov Ivan',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '9',
+        description : 'Phasellus faucibus ex ornare nibh varius, id lacinia dolor bibendum.',
+        createdAt : new Date('2018-02-23T23:00:00'),
+        author : 'Ivanov Ivan',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'health', 'anime', 'girls'],
+    },
+    {
+        id : '10',
+        description : 'Donec vel est congue, varius sapien nec, lobortis justo. Pellentesque accumsan dictum est id commodo.',
+        createdAt : new Date('2018-02-23T23:00:00'),
+        author : 'Nistuk Pavel',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '11',
+        description : 'Sed sem ex, bibendum pellentesque odio at, imperdiet tincidunt est. ',
+        createdAt : new Date('2019-02-21T10:00:00'),
+        author : 'Nistuk Pavel',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'nice', 'anime', 'girls'],
+    },
+    {
+        id : '12',
+        description : 'Ut est erat, molestie non ultricies non, accumsan eget urna.',
+        createdAt : new Date('2019-10-11T16:00:00'),
+        author : 'Nistuk Pavel',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '13',
+        description : 'Sed a suscipit ante, eu fringilla mi. In convallis dolor ut felis rhoncus finibus. ',
+        createdAt : new Date('2019-10-11T16:00:00'),
+        author : 'Nistuk Pavel',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '14',
+        description : 'auris at lorem feugiat pellentesque. Praesent ultrices, est in ',
+        createdAt : new Date('2019-10-11T16:00:00'),
+        author : 'Nistuk Pavel',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '15',
+        description : ' ut interdum ligula. Sed tempus porttitor dignissim. Nulla pulvin',
+        createdAt : new Date('2019-10-11T16:00:00'),
+        author : 'Nistuk Pavel',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '16',
+        description : 'Nam ipsum tellus, tempus vitae pellentesque quis, condimentum sit amet ligula.',
+        createdAt : new Date('2019-11-11T14:35:11'),
+        author : 'Nistuk Pavel',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '17',
+        description : 'Nam sed quam non lorem pharetra sagittis vitae a sapien.',
+        createdAt : new Date('2019-11-11T14:35:11'),
+        author : 'Vasily',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '18',
+        description : 'Sed et diam sapien. Maecenas ac dictum ipsum, nec iaculis enim.',
+        createdAt : new Date('2019-11-11T14:35:11'),
+        author : 'Vasily',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '19',
+        description : 'Proin eros lacus, sodales sed cursus nec, lobortis eu purus. Morbi vel mattis urna.',
+        createdAt : new Date('2019-11-11T14:35:11'),
+        author : 'Petya',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '20',
+        description : 'Proin eros lacus, sodales sed cursus nec, lobortis eu purus. Morbi vel mattis urna.',
+        createdAt : new Date('2019-11-11T14:35:00'),
+        author : 'Petya',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+    {
+        id : '21',
+        description : 'Proin eros lacus, sodales sed cursus nec, lobortis eu purus. Morbi vel mattis urna.',
+        createdAt : new Date('2019-11-11T14:35:00'),
+        author : 'Petya',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'anime', 'girls'],
+    },
+];
+
+let otherPosts = [
+    {
+        id : '25',
+        description : 'Nam sed quam non lorem pharetra sagittis vipsfkvmkkkkkkkkkkkkdvlk',
+        createdAt : new Date('2017-11-11T14:35:11'),
+        author : 'Vasily',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['funny', 'cats', 'girls'],
+    },
+    {
+        id : '26',
+        description : 'obortis eu purus. Morbi vel mattis urna.',
+        createdAt : new Date('2019-11-11T20:30:00'),
+        author : 'Petr',
+        photoLink : 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashTags : ['cats', 'anime', 'girls'],
+    },
+];
+
+class PhotoPostsCollection{
     constructor(photoPposts){
         this._photoPosts = photoPposts;
     }
+
+    static isEmptyString(str){
+        return (str.trim() == ' ');
+    }
     
     static validatePost(photoPost){
-       return (photoPost.description & photoPost.description.length < 200 & photoPost.id &
-        photoPost.createdAt & photoPost.author & photoPost.photoLink) 
+        if(!photoPost.hasOwnProperty('description') | typeof(photoPost.description) != 'string'
+        | PhotoPostsCollection.isEmptyString(photoPost.description) | photoPost.description.length){
+            return false;
+        }
+        if(!photoPost.hasOwnProperty('id') | typeof(photoPost.id) !== 'string' |
+         PhotoPostsCollection.isEmptyString(photoPost.id)){
+            return false;
+        }
+        if(!photoPost.hasOwnProperty('createdAt') | typeof(photoPost.createdAt) !== 'Date'){
+            return false;
+        }
+        if(!photoPost.hasOwnProperty('author') | typeof(photoPost.author) !== 'string' |
+        PhotoPostsCollection.isEmptyString(photoPost.author)){
+            return false;
+        }
+        if(!photoPost.hasOwnProperty('photoLink') | typeof(photoPost.photoLink) !== 'string' |
+        PhotoPostsCollection.isEmptyString(photoPost.photoLink)){
+            return false;
+        }
+        return true;
     }
 
+    
+
     get(id){
-        let toReturn = this._photoPosts.filter(function (element, id){
-            return (element.id == id);
-        });
-        if(validatePost(toReturn[0])){
-            return toReturn[0];
-        }
+        let toReturn = this._photoPosts.find(x => x.id === id);
+        return toReturn == null ? false : toReturn;
     }
 
     add(photoPost){
-        if(validatePost(photoPost)){
-            this._photoPosts[this._photoPosts.length] = photoPost;
+        if(PhotoPostsCollection.validatePost(photoPost)){
+            this._photoPosts.push(photoPost);
             return true;
         }else{
             return false;
@@ -36,34 +246,28 @@ class photoPostsCollection{
         return index;
     }
 
-    remove(id){
-        if(_getPostIndex(id) != -1){
-            this._photoPosts.splice(index, 1);
-            return true;
-        }else{
+    remove(id){    
+        if(post = this._photoPosts.findIndex(x => x.id === id) == -1){
             return false;
+        }else{
+            this._photoPosts.splice(post, 1);
+            return true;
         }
     }
 
-    editPhotoPost(id, photoPost){
-        let index = _getPostIndex(id);
-        if(index == -1) return false;
-        if(photoPost.description){
-            this._photoPosts[index].description = photoPost.description;
+    edit(id, photoPost){
+        let post = this.get(id);
+        if(!post) return false;
+        if(photoPost.hasOwnProperty('description') && !PhotoPostsCollection.isEmptyString(photoPost.description)){
+            post.description = photoPost.description;
         }
-        if(photoPost.photoLink){
-            this._photoPosts[index].photoLink = photoPost.photoLink;
+        if(photoPost.hasOwnProperty('photoLink') && !PhotoPostsCollection.isEmptyString(photoPost.photoLink)){
+            post.photoLink = photoPost.photoLink;
         }
-        if(photoPost.hashTags){
-            this._photoPosts[index].hashTags = photoPost.hashTags;
+        if(!photoPost.hasOwnProperty('hashTags')){
+            post.hashTags = photoPost.hashTags;
         }
-        if(photoPost.likes){
-            this._photoPosts[index].likes = photoPost.likes;
-        }
-        if(photoPost.author){
-            this._photoPosts[index].author = photoPost.author;
-        }
-        return validatePhotoPost(photoPosts[index]);
+        return validatePhotoPost(post);
     }
 
     getPage(skip = 0, top = 10, filterConfing){
@@ -77,7 +281,7 @@ class photoPostsCollection{
                 }
             }
         }
-        sortByDate(filteredPosts);
+        filteredPosts.sort((a,b) => b.createdAt - a.createdAt);
         if(filteredPosts.length - skip >= top){
             return filteredPosts.splice(skip - 1, top);
         }else{
@@ -90,14 +294,20 @@ class photoPostsCollection{
     }
 
     addAll(photoPosts){
-        let unvalidated = [];
-        for(item of photoPosts){
-            if(photoPostsCollection.validatePost(item)){
-                add(item);
-            }else{
-                unvalidated[unvalidated.length] = item;
+        let invalide = [];
+        photoPosts.forEach(element => {
+            if(!this.add(element)){
+                invalide.push(element);
+                console.log('invalid element');
+                console.log(element);
             }
-        }
-        return unvalidated;
+        });
+        return invalide;
     }
-}
+
+    
+};
+
+let photoPostsCollection = new PhotoPostsCollection(photoPosts);
+console.log(photoPostsCollection._photoPosts);
+console.log(photoPostsCollection.addAll(otherPosts));
